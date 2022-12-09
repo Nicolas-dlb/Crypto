83bd16dddac16a7e5f62155a634b69b582099b07
Nicolas Dubois <nicolas_db@outlook.fr>
2022-12-09 03:31:23 +0100

    refactor: make price change variable lowerCase

--- app/dashboard/Card.tsx
+++ app/dashboard/Card.tsx
@@ -6,7 +6,7 @@ function card(crypto: Crypto): JSX.Element {
 	function getNumberFixed(v: any, d: number): any {
 		return (Math.floor(v * Math.pow(10, d)) / Math.pow(10, d)).toFixed(d);
 	}
-	const PriceChangeColor =
+	const priceChangeColor =
 		crypto?.price_change_percentage_24h > 0 ? "text-rose-500" : "text-teal-500";
 
 	return (
@@ -29,7 +29,7 @@ function card(crypto: Crypto): JSX.Element {
 				<p>{getNumberFixed(crypto?.current_price, 2)}$</p>
 			</div>
 			<div
-				className={`${PriceChangeColor} w-1/6 m-0 p-0 items-center flex h-fit`}
+				className={`${priceChangeColor} w-1/6 m-0 p-0 items-center flex h-fit`}
 			>
 				<p> {getNumberFixed(crypto?.price_change_percentage_24h, 2)}%</p>
 			</div>
