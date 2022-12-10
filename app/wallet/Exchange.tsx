fa3aa24b5abd4e0bba7fe0393888d1418e29a676
Nicolas Dubois <nicolas_db@outlook.fr>
2022-12-10 14:11:58 +0100

    feat: prevent letters for the exchange input

--- app/wallet/Exchange.tsx
+++ app/wallet/Exchange.tsx
@@ -8,14 +8,16 @@ function Exchange() {
 		<div className="bg-slate-100 h-40 flex-col flex justify-between rounded-md p-4">
 			<div className="text-start">
 				<h3 className="text-sm text-slate-600">Swap</h3>
-				<div className="bg-white rounded-md flex justify-between w-full">
+				<div className="bg-white rounded-md flex justify-between pl-2 w-full">
 					<input
 						onKeyPress={isNumberKey}
 						onChange={(e) => setInput(e.target.value)}
 						type="number"
 						min="0"
 						pattern="[0-9\.]+"
-						className="bg-transparent w-full outline-none"
+						step="0.01"
+						placeholder="Enter an amount"
+						className="bg-transparent w-full outline-none appearance-none"
 					/>
 					<select className="rounded-md outline-none">
 						<option value="Bitcoin">BTC</option>
