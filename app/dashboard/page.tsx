67e77dab73bf321db753a8fc36756e32e4fc4f5b
Nicolas Dubois <nicolas_db@outlook.fr>
2022-12-14 20:49:25 +0100

    feat: change card keys to crypto id

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,9 +7,9 @@ async function Dashboard() {
 	const cryptos: Crypto[] = await fetchCryptos();
 
 	return (
-		<div className=" overflow-scroll md:w-full flex-col justify-start custom-height-dashboard md:h-full  md:top-0">
-			{cryptos?.map((crypto: Crypto, index) => (
-				<Card key={index} {...crypto} />
+		<div className=" overflow-scroll md:w-full flex-col justify-start h-[94vh] md:h-full  md:top-0">
+			{cryptos?.map((crypto: Crypto) => (
+				<Card key={crypto.id} {...crypto} />
 			))}
 		</div>
 	);
