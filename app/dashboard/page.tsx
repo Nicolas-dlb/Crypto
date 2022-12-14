adb1718b564d4f079ef70da6829c6959bca36eda
Nicolas Dubois <nicolas_db@outlook.fr>
2022-12-14 22:04:07 +0100

    style: add padding to dashboard for mobile

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,7 +7,7 @@ async function Dashboard() {
 	const cryptos: Crypto[] = await fetchCryptos();
 
 	return (
-		<div className=" overflow-scroll md:w-full flex-col justify-start h-[94vh] md:h-full  md:top-0">
+		<div className=" overflow-scroll md:w-full flex-col justify-start pt-2 md:pt-0 h-[94vh] md:h-full  md:top-0">
 			{cryptos?.map((crypto: Crypto) => (
 				<Card key={crypto.id} {...crypto} />
 			))}
