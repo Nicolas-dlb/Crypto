1cce1d76887bc3a4e8d6850f9b470bccc1b63aa3
Nicolas Dubois <nicolas_db@outlook.fr>
2022-12-26 04:51:58 +0100

    feat: improve navbar responsive

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -4,7 +4,7 @@ import NavbarLink from "./components/NavbarLink";
 
 function Navbar() {
 	return (
-		<nav className="flex md:justify-start z-10 sticky w-full min-w-400 justify-around md:top-0 bg-slate-100 shadow md:flex-col md:w-60 rounded-t-md md:h-screen box-border">
+		<nav className="flex md:justify-start z-10 sticky w-full justify-around md:top-0 bg-slate-100 shadow md:flex-col md:min-w-[200px] md:max-w-[200px] rounded-t-md md:h-screen box-border">
 			<NavbarLink categorie="Dashboard" />
 			<NavbarLink categorie="Wallet" />
 			<NavbarLink categorie="Settings" />
