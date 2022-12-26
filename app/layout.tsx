3c6c4d72e69b0f9ff7ad20622b30fc7691f9a033
Nicolas Dubois <nicolas_db@outlook.fr>
2022-12-26 03:39:33 +0100

    feat: add font smoothing

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 			<head />
 
 			<body
-				className={`${inter.variable} font-sans bg-slate-300 text-sm overflow-hidden flex h-screen box-border m-w-350 justify-between md:justify-start flex-col-reverse md:flex-row`}
+				className={`${inter.variable} antialiased font-sans bg-slate-300 text-sm overflow-hidden flex h-screen box-border m-w-350 justify-between md:justify-start flex-col-reverse md:flex-row`}
 			>
 				<ReduxProvider>
 					<Navbar />
