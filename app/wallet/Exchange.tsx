02d5e9f7a4f85c22bb8e22fcce733432976a9a52
Nicolas Dubois <nicolas_db@outlook.fr>
2022-12-28 06:18:22 +0100

    feat: add style to exchange

--- app/wallet/Exchange.tsx
+++ app/wallet/Exchange.tsx
@@ -4,8 +4,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { getMarket } from "../../redux/reducers/marketSlice";
 import { selectWallet, swapToken } from "../../redux/reducers/walletSlice";
 import { Crypto } from "../../typing";
-import { fetchCryptos, isNumberKey } from "../../utils";
-import Dropdown from "../settings/Dropdown";
+import { classNames, fetchCryptos, isNumberKey } from "../../utils";
+import Label from "../components/Label";
+import { button } from "../styles/globals";
+import { input } from "../styles/exchange";
+import Dropdown from "./Dropdown";
+import Error from "../components/Error";
 
 function Exchange() {
 	const [amountToSell, setAmountToSell] = useState<number | string>("");
@@ -15,16 +19,7 @@ function Exchange() {
 	const [tokenToBuyName, setTokenToBuyName] = useState("ethereum");
 	const wallet = useSelector(selectWallet);
 	const inputRef = useRef<HTMLInputElement>(null);
-	const [error, setError] = useState(false);
-
-	useEffect(() => {
-		fetchCryptos().then((data) => {
-			if (data !== market && data) {
-				setMarket(data);
-				dispatch(getMarket(data));
-			}
-		});
-	}, []);
+	const [insufficientTokens, setInsufficientTokens] = useState(false);
 
 	const tokenToSell = market.find(
 		(token) => token.name.toLowerCase() == tokenToSellName
@@ -34,6 +29,15 @@ function Exchange() {
 		(token) => token.name.toLowerCase() == tokenToBuyName
 	);
 	let amountToBuy = price / (tokenToBuy?.current_price as number);
+
+	const step =
+		wallet[tokenToSellName] < 0.001
+			? "0.00001"
+			: wallet[tokenToSellName] < 0.01
+			? "0.0001"
+			: wallet[tokenToSellName] < 3
+			? "0.01"
+			: "0.1";
 
 	const swapTokens = (e: MouseEvent): void => {
 		if (amountToSell) {
@@ -48,72 +52,67 @@ function Exchange() {
 				);
 				setAmountToSell("");
 			} else {
-				setError(true);
+				setInsufficientTokens(true);
 				setTimeout(() => {
-					setError(false);
+					setInsufficientTokens(false);
 				}, 2000);
 			}
 		}
 	};
 
 	useEffect(() => {
+		fetchCryptos().then((data) => {
+			if (data !== market && data) {
+				setMarket(data);
+				dispatch(getMarket(data));
+			}
+		});
+	}, []);
+
+	useEffect(() => {
 		setAmountToSell(inputRef.current!.value);
 	}, [inputRef.current?.value]);
 
 	return (
-		<div className="bg-slate-100 h-1/3 flex-col flex justify-between rounded-md p-4">
-			<div className="text-start">
-				<div className="flex w-[150px] justify-between">
-					<h3 className="text-sm xs:text-xs mb-2 text-slate-600">Swap</h3>
-					<p className="text-rose-700 text-[12.5px]">
-						{error && "Insufisant wallet"}
-					</p>
-				</div>
-				<div className="bg-white rounded-md h-8 flex justify-between w-full">
-					<input
-						onKeyPress={isNumberKey}
-						onChange={(e) => setAmountToSell(e.target.value)}
-						ref={inputRef}
-						type="number"
-						min="0"
-						max={wallet[tokenToSellName]}
-						step={
-							wallet[tokenToSellName] < 0.001
-								? "0.00001"
-								: wallet[tokenToSellName] < 0.01
-								? "0.0001"
-								: wallet[tokenToSellName] < 3
-								? "0.01"
-								: "0.1"
-						}
-						value={amountToSell}
-						placeholder="Enter an amount"
-						className="bg-transparent w-full h-full  rounded-sm pl-2 outline-none"
-					/>
-					<Dropdown
-						state={tokenToSellName}
-						setState={setTokenToSellName}
-						options={wallet}
-						tokenToSellName={tokenToSellName}
-					/>
-				</div>
-				<h3 className="text-sm my-1 xs:text-xs text-slate-600">For</h3>
-				<div className="mt-2 pl-2 bg-slate-200 h-8 flex justify-between w-full rounded-md">
-					<p className="w-full items-center flex text-slate-400">
-						{amountToBuy > 0 && amountToBuy.toFixed(5)}
-					</p>
-					<Dropdown
-						state={tokenToBuyName}
-						setState={setTokenToBuyName}
-						options={market}
-						tokenToSellName={tokenToSellName}
-					/>
-				</div>
+		<div className="bg-slate-100 shadow flex-col flex justify-between rounded-md p-4">
+			<div className="flex w-[160px] justify-between">
+				<Label className="mb-2">Swap</Label>
+				<Error>{insufficientTokens && "Insufficient tokens"}</Error>
 			</div>
-			<button
-				onClick={swapTokens}
-				className="bg-slate-300 text-white rounded-md w-full h-8 mt-4"
-			>
+			<div className="pl-2 h-8 flex justify-between w-full rounded-md bg-white">
+				<input
+					onKeyPress={isNumberKey}
+					onChange={(e) => setAmountToSell(e.target.value)}
+					ref={inputRef}
+					type="number"
+					min="0"
+					max={wallet[tokenToSellName]}
+					step={step}
+					value={amountToSell}
+					placeholder="Enter an amount"
+					className={input}
+				/>
+				<Dropdown
+					state={tokenToSellName}
+					setState={setTokenToSellName}
+					options={wallet}
+					tokenToSellName={tokenToSellName}
+				/>
+			</div>
+			<Label className="my-2">For</Label>
+			<div className="pl-2 h-8 flex justify-between w-full rounded-md bg-slate-200">
+				<p className="items-center flex text-slate-400">
+					{amountToBuy > 0 && amountToBuy.toFixed(5)}
+				</p>
+				<Dropdown
+					state={tokenToBuyName}
+					setState={setTokenToBuyName}
+					options={market}
+					tokenToSellName={tokenToSellName}
+				/>
+			</div>
+
+			<button onClick={swapTokens} className={classNames(button, "mt-4")}>
 				Exchange
 			</button>
 		</div>
